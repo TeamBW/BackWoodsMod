f1addb09768e77f36f63c6da290dad3f998bea0f
Christophernjsp <minecraftandmore86@gmial.com>
2014-10-22 11:48:36 -0400

    Syncing

--- src/main/java/com/chaka/thebackwoods/block/BlockAmethyst.java
+++ src/main/java/com/chaka/thebackwoods/block/BlockAmethyst.java
@@ -1,6 +1,8 @@
 package com.chaka.thebackwoods.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockAmethyst extends BlockTBW {
 
@@ -9,5 +11,10 @@ public class BlockAmethyst extends BlockTBW {
         super(Material.iron);
         this.setBlockName("amethyst");
         this.setStepSound(soundTypeMetal);
+    }
+
+    public boolean isBeaconBase(IBlockAccess worldObj, int x, int y, int z, int beaconX, int beaconY, int beaconZ) {
+
+        return this == Blocks.emerald_block || this == Blocks.gold_block || this == Blocks.diamond_block || this == Blocks.iron_block;
     }
 }
---
+++ src/main/java/com/chaka/thebackwoods/init/Recipies.java
@@ -0,0 +1,7 @@
+package com.chaka.thebackwoods.init;
+
+/**
+ * Created by Christopher on 10/21/2014.
+ */
+public class Recipies {
+}
